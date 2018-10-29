54be7c49f277d2dfe80d78f232f0b2a0236e8f77
GreatWolf666 <nagy.kristof94@gmail.com>
2018-10-29 15:35:27 +0100

    Added 2 more completed and 2 incomplete java exercises

---
+++ week-01/day-3/Java/src/DefineBasicInfo.java
@@ -0,0 +1,26 @@
+public class DefineBasicInfo {
+
+    public static void main(String[] args) {
+
+        String myName = "Nagy Kristof";
+        int myAge = 24;
+
+        double myHeight = 1.67;
+
+        System.out.println("My name is "+ myName);
+        System.out.println("My age is "+myAge );
+        System.out.println("My height is "+ myHeight);
+
+        boolean married = true;
+
+        if ( married == true) {
+            System.out.println("I am married.");
+        }
+        else {
+            System.out.println("I'm not married.");
+        }
+
+
+
+    }
+}
---
+++ week-01/day-3/Java/src/VariableMutations.java
@@ -0,0 +1,7 @@
+public class VariableMutations {
+
+
+    public static void main(String[] args) {
+
+    }
+}
