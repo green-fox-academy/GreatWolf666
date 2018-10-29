a08a4fcc684f6abe810152bf937f91562636f1b5
GreatWolf666 <nagy.kristof94@gmail.com>
2018-10-29 12:35:18 +0100

    First 3 Java excercises

---
+++ week-01/day-3/Java/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Adam!");
+        System.out.println("Hello, Csirke!");
+        System.out.println("Hello, Duffy!");
+    }
+}
---
+++ week-01/day-3/Java/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Me!");
+        // I didn't have a better witty text so... Nope just go about your business.
+    }
+}
