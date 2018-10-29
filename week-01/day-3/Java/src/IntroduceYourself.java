a8e2bc88f563cec21bf3b7e8a417c034b66f9fb9
GreatWolf666 <nagy.kristof94@gmail.com>
2018-10-29 14:09:28 +0100

    2 new Java files added.

---
+++ week-01/day-3/Java/src/IntroduceYourself.java
@@ -0,0 +1,12 @@
+public class IntroduceYourself {
+
+    public static void main(String[] args) {
+
+        System.out.println("Nagy Kristof");
+        System.out.println("I am 24 years old");
+        System.out.println("1.68");
+
+
+    }
+
+}
---
+++ week-01/day-3/Java/src/TwoNumbers.java
@@ -0,0 +1,18 @@
+public class TwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 13;
+        int b = 22;
+        double c = 13.0;
+        double d = 22.0;
+
+        System.out.println(b + a);
+        System.out.println(a - b);
+        System.out.println(b * a);
+        System.out.println(d / c);
+        System.out.println(b / a);
+        System.out.println(b % a);
+
+    }
+}
