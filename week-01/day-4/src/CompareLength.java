b04000df618c3564ca5d325c4ce50f1a8dc9a29d
GreatWolf666 <nagy.kristof94@gmail.com>
2018-10-30 17:38:38 +0100

    Some more Javas to the Sand Crawler!

---
+++ week-01/day-4/src/CompareLength.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class CompareLength {
+
+    public static void main(String[] args) {
+
+        int p1[] = {1, 2, 3};
+        int length = p1.length;
+        int p2[] = {4, 5};
+        int length2 = p2.length;
+
+        if (p2.length > p1.length )
+            System.out.println("p2 has more elements.");
+         else
+            System.out.println("p1 has more elements.");
+
+
+    }
+
+
+}
---
+++ week-01/day-4/src/SumElements.java
@@ -0,0 +1,27 @@
+public class SumElements {
+
+    public static void main(String[] args) {
+
+
+        int[] r;
+        r = new int[4];
+
+        r[0] = 54;
+        r[1] = 23;
+        r[2] = 66;
+        r[3] = 12;
+
+//        int[] r = {54, 23, 66, 12};
+//
+        int sum = 0;
+//
+        for(int num: r) {
+            sum = r[2] + r[3];
+//
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
