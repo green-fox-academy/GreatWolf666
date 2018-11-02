7b181cec966ca230ea605d338ca8ee8601a12168
GreatWolf666 <nagy.kristof94@gmail.com>
2018-11-02 20:25:54 +0100

    Added a whole bunch of changes, detailed list will come, too tired now.

---
+++ week-01/day-4/src/SumAll.java
@@ -0,0 +1,16 @@
+public class SumAll {
+
+    public static void main(String[] args) {
+
+        int[] ai = {3,4,5,6,7};
+        int a = 0;
+
+        for(int i = 0; i < ai.length; ++i) {
+            a = a + ai[i];
+
+        }
+        System.out.println(a);
+
+
+    }
+}
