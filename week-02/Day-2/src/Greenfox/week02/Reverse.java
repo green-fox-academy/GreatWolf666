fcd7c6349496ee5d6a1c137b2de3d7060bff7940
GreatWolf666 <nagy.kristof94@gmail.com>
2018-11-06 17:25:33 +0100

    Added comment to Reverse exercise, nothing else new yet

--- week-02/Day-2/src/Greenfox/week02/Reverse.java
+++ week-02/Day-2/src/Greenfox/week02/Reverse.java
@@ -2,13 +2,13 @@ package Greenfox.week02;
 
 public class Reverse {
     public static void main(String[] args) {
+//        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
 
         StringBuilder reversed = new StringBuilder(".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI");
 
-//        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
         System.out.println(reversed.reverse());
 
-
+//   Will come back with more solutions if they come to mind.
 
     }
 }
