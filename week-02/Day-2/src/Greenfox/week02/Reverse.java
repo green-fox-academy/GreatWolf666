a98c1ca01291eeac3c989200f1093d26b8b09830
GreatWolf666 <nagy.kristof94@gmail.com>
2018-11-06 14:46:16 +0100

    one more to the saltmines

---
+++ week-02/Day-2/src/Greenfox/week02/Reverse.java
@@ -0,0 +1,14 @@
+package Greenfox.week02;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        StringBuilder reversed = new StringBuilder(".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI");
+
+//        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+        System.out.println(reversed.reverse());
+
+
+
+    }
+}
