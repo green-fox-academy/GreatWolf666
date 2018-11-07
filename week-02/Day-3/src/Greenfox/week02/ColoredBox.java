5c7e78d43887a4f32d06f63a4f0c138b904f08db
GreatWolf666 <nagy.kristof94@gmail.com>
2018-11-07 11:36:15 +0100

    Day 3 first commit, 2 exercises completed

---
+++ week-02/Day-3/src/Greenfox/week02/ColoredBox.java
@@ -0,0 +1,52 @@
+package Greenfox.week02;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+
+//        graphics.setColor(Color.GREEN);
+//        graphics.drawLine(50, 50, );
+
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(50, 50, 150, 50);
+
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(50, 100, 150, 100);
+
+        graphics.setColor(Color.yellow);
+        graphics.drawLine(50, 100, 50, 50);
+
+        graphics.setColor(Color.MAGENTA);
+        graphics.drawLine(150, 100, 150, 50);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
