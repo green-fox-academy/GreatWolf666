95395ae5e12ef11209fb8b19d989cb5d66e08069
GreatWolf666 <nagy.kristof94@gmail.com>
2018-11-15 12:29:04 +0100

    Week-3 Day-4, 2 more exercises done

---
+++ Week-03/Day-4/src/Greenfox/week03/Bunny1.java
@@ -0,0 +1,14 @@
+package Greenfox.week03;
+
+public class Bunny1 {
+  public static void main(String[] args) {
+    System.out.println(bunnyEarCounter(10, 2)); // This one prints out the input / equation
+
+  }
+  public static int bunnyEarCounter(int bunnies, int ears){
+    if (bunnies == 0){ //This stops the call
+      return 0;
+    }
+    return bunnyEarCounter(bunnies -1, ears) + ears; //bunnies -1 makes it count forward
+  }
+}
