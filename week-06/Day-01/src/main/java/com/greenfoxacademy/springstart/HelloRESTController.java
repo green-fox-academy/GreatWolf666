2b4f087d14264be7f62b7ad08a254aaa4e85c5f3
GreatWolf666 <nagy.kristof94@gmail.com>
2018-12-05 15:51:35 +0100

    The HelloWebController exercise not yet finished but getting pushed nonetheless

--- week-06/Day-01/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
+++ week-06/Day-01/src/main/java/com/greenfoxacademy/springstart/HelloRESTController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+
+  AtomicLong atomicLong = new AtomicLong(1);
+
   @RequestMapping(value = "/greeting")
   @ResponseBody
   public Greeting greeting(@RequestParam() String name) {
-    return new Greeting(1,name);
+    return new Greeting(atomicLong.incrementAndGet(),name);
   }
 
 
