41ea63a8d300500732298e917e7a5e133717918c
GreatWolf666 <nagy.kristof94@gmail.com>
2018-12-13 11:55:02 +0100

    Project created for DI workshop

---
+++ week-07/Day-4/src/main/java/com/greenfoxacademy/diworkshop/CommandLineRunner.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.diworkshop;
+
+public interface CommandLineRunner {
+}
---
+++ week-07/Day-4/src/main/java/com/greenfoxacademy/diworkshop/DiworkshopApplication.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.diworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiworkshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiworkshopApplication.class, args);
+  }
+
+}
+
