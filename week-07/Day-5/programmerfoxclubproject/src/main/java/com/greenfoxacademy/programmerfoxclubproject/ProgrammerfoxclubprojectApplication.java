c8d9a8ba3ece2bd1b1f6e1e806153aa48238b985
GreatWolf666 <nagy.kristof94@gmail.com>
2018-12-14 11:20:14 +0100

    Project created

---
+++ week-07/Day-5/programmerfoxclubproject/src/main/java/com/greenfoxacademy/programmerfoxclubproject/ProgrammerfoxclubprojectApplication.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.programmerfoxclubproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammerfoxclubprojectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProgrammerfoxclubprojectApplication.class, args);
+  }
+
+}
+
